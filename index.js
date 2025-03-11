45bb57a2833663408df4f64851c200f961c77579
MarvinDanielOduor <marvin.ouma@strathmore.edu>
2025-03-11 12:42:34 +0300

    Completed Arrays Lab (CodeGrade)

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write your solution here!
+// index.js
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+// Optional export for testing
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = {
+    cats,
+    destructivelyAppendCat,
+    destructivelyPrependCat,
+    destructivelyRemoveLastCat,
+    destructivelyRemoveFirstCat,
+    appendCat,
+    prependCat,
+    removeLastCat,
+    removeFirstCat,
+  };
+}
